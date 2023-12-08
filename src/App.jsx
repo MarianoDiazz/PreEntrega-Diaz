b17769e5fd0d14f57e186bf9414082785e6b810e
Mariano Diaz <marianod9797@gmail.com>
2023-12-07 21:02:55 -0300

    change styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Components/Navigation';
 import ItemListContainer from './Components/ItemListContainer';
 function App() {
-  const greetingMessage = '¡Bienvenido a nuestra tienda en línea!';
+  const greeting = '¡Bienvenido a nuestra tienda en línea!';
   return (
     <>
       <Navigation />
-      <ItemListContainer greeting={greetingMessage} />    </>
+      <ItemListContainer greeting={greeting} />    </>
   )
 }
 
